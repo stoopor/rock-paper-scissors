10b11744e6a849065ded92ea58e761b9e664d9a5
Stoo <stufg@protonamil.com>
2022-07-19 22:01:01 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,95 @@
+const choices = ["rock", "paper", "scissors"];
+let paragraph = document.getElementById("gameDisplay");
+let computerIndex = -1; 
+        function getComputerChoice()
+        {
+            let choice = Math.floor(Math.random() * 3);
+            return choice;
+        }
+        
+
+        function getPlayerChoice(compIndex)
+        {
+            let good = false;
+            let index = -1;
+            while(!good)
+            {
+                let choice = prompt("Type your choice. Either: Rock, Paper, Scissors");
+                if (choice === null || choice === undefined) continue;
+            choice = choice.toLowerCase();
+            index = choices.indexOf(choice);
+            if(index === computerIndex){
+                alert("Tie, Pick Again!");
+                computerIndex = getComputerChoice();
+                compIndex = computerIndex;
+                continue;
+            }
+            if(index != -1) good = true;
+            }
+            return index;
+        }
+
+        function gameLoop()
+        {
+            let score = 0;
+            let computerScore = 0;
+            paragraph.innerHTML+="<br>";
+            for(let i = 0; i < 5; i++)
+            {
+                computerIndex = getComputerChoice();
+                let index = getPlayerChoice(computerIndex);
+                switch(index)
+                {
+                    case 0: 
+                    if(computerIndex === 1)
+                    {
+                        alert("computer chose paper, you lose!");
+                        paragraph.innerHTML+="<br>";
+                        computerScore++;
+                    }
+                    else if(computerIndex === 2)
+                    {
+                        alert("computer chose scissors, you win!");
+                        paragraph.innerHTML+="<br>";
+                        score++;
+                    }
+                    break;
+                    case 1:
+                    if(computerIndex === 0)
+                    {
+                        alert("computer chose rock, you win!");
+                        paragraph.innerHTML+="<br>";
+                        score++;
+                    }
+                    else if(computerIndex === 2)
+                    {
+                        alert("computer chose scissors, you lose!");
+                        paragraph.innerHTML+="<br>";
+                        computerScore++;
+                    }
+                    break;
+                    case 2:
+                        if(computerIndex === 0)
+                        {
+                            alert("computer chose rock,you lose!");
+                            paragraph.innerHTML+="<br>";
+                            computerScore++;
+                        }
+                        else if(computerIndex === 1)
+                        {
+                            alert("computer chose paper,you win!");
+                            paragraph.innerHTML+="<br>";
+                            score++;
+                        }
+                        break;
+                    default:;
+                }
+                
+            }
+    
+            if(score === computerScore) alert("THE SERIES IS A DRAW!");
+                else if(score > computerScore) paragraph.innerText+=("You won the series!");
+                alert("You lost the series!");
+        }
+        
+        
